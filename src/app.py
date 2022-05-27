56f6c4ffe5d65228a8a83b200cc3095a5285eb1a
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-05-27 08:37:38 +0000

    Ejemplo Sencillo Flask API

--- src/app.py
+++ src/app.py
@@ -25,18 +25,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+@app.route('/members/<int:member_id>', methods=['GET'])
+def miembro_familia(member_id):
 
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    member = jackson_family.get_member(member_id)
+    response_body = member
     return jsonify(response_body), 200
+
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def miembro_a_borrar(member_id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(member_id)
+    response_body = member
+    return jsonify(response_body), 200
+
+
+@app.route('/members', methods=['GET'])
+def handle_hello():
+    body = request.get_json()
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+    print(members) #solo para testear
+    response_body = members
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def test_post():    
+    # this is how you can use the Family datastructure by calling its methods
+
+    body = request.get_json() #PARA LEER LOS DATOS ENVIADOS DESDE EL POST
+    #Asignamos los valores
+    member = {
+        "first_name": body['first_name'],
+        "last_name": body['last_name'],
+        "age": body['age'],        
+        "lucky_numbers": body['lucky_numbers']
+    }  
+    #llamamos al metodo add_member
+    jackson_family.add_member(member)
+    #mostramos la respuesta
+    return jsonify(member), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,29 +8,49 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
-class FamilyStructure:
-    def __init__(self, last_name):
+class FamilyStructure: #DEFINIMOS LA CLASE FAMILIA CON SU ESTRUCTURA
+    def __init__(self,last_name):      
         self.last_name = last_name
-
-        # example list of members
         self._members = []
-
+        # example list of members
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
-        # fill this method and update the return
-        pass
-
+    def add_member(self, member):        
+       # fill this method and update the return
+       #Generamos el id , utilizando el metodo generateid
+       #member['id'] = _generateId()
+       #añadimos a _members el elemento
+       member['id'] = self._generateId()
+       self._members.append(member)
+       print(member)
+        
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        print("estamops en delete_member - id por parametro = " + str(id))
+        #Recorremos el array de elementos _selfmembers
+        for indice in range(0,len(self._members)):  
+            #si el indice que contiene el id es igual al id recibido
+            if self._members[indice]['id'] == id: 
+                #eliminamos ese elemento del array
+                self._members.pop(indice)
+                print(self._members)
+        return self._members
+
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        print("estamops en get_member - id por parametro = " + str(id))        
+        
+        for people in self._members:  
+            if people['id'] == id:
+                print(people)# Para testear print(lst.index(20))
+                #print(self._members.index(people[id]))
+            return people
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
